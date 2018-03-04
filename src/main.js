4ad490fd9ed3bd834913851d4d956109acb36c3b
Rain <276161983@qq.com>
2018-03-04 23:49:08 +0800

    34

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引用API文件
+import api from '@/api/axios'
 
 Vue.config.productionTip = false
-
+// 将API方法绑定到全局
+Vue.prototype.$api = api
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
